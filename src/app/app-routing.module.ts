f82b2f9587773f883daf8592be4042e3d1fdb99b
William Mosquera <wjmmk3@gmail.com>
2022-12-06 14:04:29 -0500

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './core/shared/components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', loadChildren: () => import('./public/public.module').then(m => m.PublicModule)},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/public/public.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../core/shared/shared.module';
+import { HomeComponent } from './home/containers/home.component';
+import { LoginComponent } from './login/containers/login.component';
+import { PublicRoutingModule } from './public-routing.module';
+import { PublicComponent } from './public.component';
+
+@NgModule({
+    imports: [
+        PublicRoutingModule,
+        SharedModule
+    ],
+    declarations:[
+        HomeComponent,
+        PublicComponent,
+        LoginComponent
+    ],
+    exports: [],
+    providers: []
+})
+
+export class PublicModule {
+    constructor(){}
+}
